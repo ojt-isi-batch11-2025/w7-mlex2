30db325fb0804ddee54b412b7eb15ee36e737124
Timothy James Concepcion <timothyjtc@outlook.com>
2025-06-20 23:59:17 +0800

    add PYTHON_VERSION variable in envinit

--- .envinit.sh
+++ .envinit.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+PYTHON_VERSION="3.10.13"
 # if .venv does not exist, then create it
 if [ ! -d ".venv" ]; then
-    uv venv .venv --python=3.10
+    uv venv .venv --python=${PYTHON_VERSION}
 fi
 # if which python does not return the path to the virtual environment's python executable, then activate the virtual environment
 if [ "$(which python)" != "$(readlink -f .venv/Scripts/python)" ]; then
